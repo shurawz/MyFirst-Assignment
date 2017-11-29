93e8b01760c8c515d61a2ab3d74e051021161ba9
shurawz <33598462+shurawz@users.noreply.github.com>
2017-11-29 13:04:42 +0545

    We've done the project to find the square numbers

---
+++ table of square numbers/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+
+    int i = 1 , x , y ;
+printf("n n.sq\n");
+printf("  \n");
+    while (i<=10)
+    {
+       x = i ;
+         y = i*i ;
+
+         printf("%d %d \n",x ,y );
+         i = i + 1;
+    }
+    return 0;
+}
